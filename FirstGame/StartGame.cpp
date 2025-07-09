24e47886a05f5a642cf490cfb03525f45ef12e74
Disthinker <2511366326@qq.com>
2025-07-09 21:48:59 +0800

    Disthinker: class for enemy and bullet enabled.

--- FirstGame/StartGame.cpp
+++ FirstGame/StartGame.cpp
@@ -6,16 +6,13 @@
 const int WINDOW_WIDTH{ 1280 };
 const int WINDOW_HEIGHT{ 720 };
 
-// const def player anim num 6
-const int PLAYER_ANIM_NUM{ 6 };
-
 // def img objects global
 IMAGE img_background;
 
 // load MSIMG32.LIB for transparent display
 #pragma comment(lib,"MSIMG32.LIB")
 // encaposlt putimage to enable transparent
-inline void putimage_alpha(int x, int y, IMAGE* img)
+inline const void putimage_alpha(int x, int y, IMAGE* img)
 {
 	int w{ img->getwidth() };
 	int h{ img->getheight() };
@@ -87,7 +84,7 @@ private:
 	std::vector<IMAGE*> frame_list;
 };
 
-// def player class(passin )
+// def player class(no pass in, default is ok)
 class Player
 {
 public:
@@ -198,6 +195,11 @@ public:
 			anim_right->Play(position.x, position.y, delta);
 	}
 
+	const POINT& GetPosition() const
+	{
+		return position;
+	}
+
 private:
 	// def const properties of player
 	const int SPEED{ 5 };
@@ -215,6 +217,140 @@ private:
 	bool is_move_right{ false };
 };
 
+// def Bullet
+class Bullet
+{
+public:
+	Bullet() = default;
+	~Bullet() = default;
+	POINT position{ 0,0 };
+
+	void Draw() const
+	{
+		// color of fill circle and draw 
+		setlinecolor(RGB(255, 155, 50));
+		setfillcolor(RGB(200, 75, 10));
+		fillcircle(position.x, position.y, RADIUS);
+	}
+
+private:
+	// RADIUS^2 PI = S
+	const int RADIUS = 10;
+};
+
+class Enemy
+{
+public:
+	// constructor like Player
+	Enemy()
+	{
+		loadimage(&img_shadow, _T("../Tivat/img/shadow_enemy.png"));
+		anim_left = new Animation(_T("../Tivat/img/enemy_left_%d.png"), 6, 45);
+		anim_right = new Animation(_T("../Tivat/img/enemy_right_%d.png"), 6, 45);
+
+		// edge of generating enemy
+		enum class SpawnEdge
+		{
+			Up = 0,
+			Down,
+			Left,
+			Right
+		};
+
+		// randomly place the enemy outside the map near the edge
+		SpawnEdge edge{ (SpawnEdge)(rand() % 4) };
+		switch (edge)
+		{
+		case SpawnEdge::Up:
+			position.x = rand() % WINDOW_WIDTH;
+			position.y = -FRAME_HEIGHT;
+			break;
+		case SpawnEdge::Down:
+			position.x = rand() % WINDOW_WIDTH;
+			position.y = WINDOW_HEIGHT;
+			break;
+		case SpawnEdge::Left:
+			position.x = -FRAME_HEIGHT;
+			position.y = rand() % WINDOW_HEIGHT;
+			break;
+		case SpawnEdge::Right:
+			position.x = WINDOW_WIDTH;
+			position.y = rand() % WINDOW_HEIGHT;
+			break;
+		default:
+			break;
+		}
+	}
+
+	bool CheckBulletCollision(const Bullet& bullet)
+	{
+		return false;
+	}
+
+	bool CheckPlayerCollision(const Player& player)
+	{
+		return false;
+	}
+
+	void Move(const Player& player)
+	{
+		const POINT& player_position = player.GetPosition();
+		int dir_x = player_position.x - position.x;
+		int dir_y = player_position.y - position.y;
+		double len_dir = sqrt(dir_x * dir_x + dir_y * dir_y);
+		if (len_dir != 0)
+		{
+			double normalized_x = dir_x / len_dir;
+			double normalized_y = dir_y / len_dir;
+			position.x += (int)(SPEED * normalized_x);
+			position.y += (int)(SPEED * normalized_y);
+		}
+
+		if (dir_x < 0)
+			facing_left = true;
+		else if (dir_x > 0)
+			facing_left = false;
+	}
+
+	void Draw(int delta)
+	{
+		int pos_shadow_x = position.x + (FRAME_WIDTH / 2 - SHADOW_WIDTH / 2);
+		int pos_shadow_y = position.y + FRAME_HEIGHT - 35;
+		putimage_alpha(pos_shadow_x, pos_shadow_y, &img_shadow);
+
+		if (facing_left)
+			anim_left->Play(position.x, position.y, delta);
+		else
+			anim_right->Play(position.x, position.y, delta);
+	}
+
+	// destructor like Player
+	~Enemy()
+	{
+		delete anim_left;
+		delete anim_right;
+	}
+
+private:
+	const int SPEED{ 3 };
+	const int FRAME_WIDTH{ 80 };
+	const int FRAME_HEIGHT{ 80 };
+	const int SHADOW_WIDTH{ 48 };
+
+	IMAGE img_shadow;
+	Animation* anim_left;
+	Animation* anim_right;
+	POINT position{ 0,0 };
+	bool facing_left{ false };
+};
+
+void TryGenerateEnemy(std::vector<Enemy*>& enemy_list)
+{
+	const int INTERVAL{ 100 };
+	static int counter{ 0 };
+	if ((++counter) % INTERVAL == 0)
+		enemy_list.push_back(new Enemy());
+}
 
 int main()
 {
@@ -237,6 +373,9 @@ int main()
 	//init player
 	Player player1{};
 
+	//inti enemy
+	std::vector<Enemy*> enemy_list;
+
 	// batch draw for drawing smoothly
 	BeginBatchDraw();
 
@@ -254,14 +393,23 @@ int main()
 		// get new position of player
 		player1.Move();
 
+		// generate enemy
+		TryGenerateEnemy(enemy_list);
+		for (Enemy* enemy : enemy_list)
+			enemy->Move(player1);
+
 		// clear screen
 		cleardevice();
 
 		// render background
 		putimage(0, 0, &img_background);
 
-		// draw player anim
+		// draw player and enemy anim
 		player1.Draw(1000 / 180);
+		for (Enemy* enemy : enemy_list)
+		{
+			enemy->Draw(1000 / 180);
+		}
 
 		FlushBatchDraw();
 
